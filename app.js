a57f324573674552e7d326b8435c5c37eec083cd
Roel Voordendag <rvoordendag@gmail.com>
2019-06-08 13:14:53 +0200

    changed some stuff and added a get

--- app.js
+++ app.js
@@ -1,61 +1,46 @@
-const express = require('express');
-const app = express()
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const spotify = require('./utils/spotify');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const spotify = require("./utils/spotify");
 
-console.log(spotify.checkArtist('Slaves'));
+console.log(spotify.checkArtist("Slaves"));
 
 //setup connection with server
-mongoose.connect('mongodb://localhost/gigShareApi');
+mongoose.connect("mongodb://localhost/gigShareApi");
 mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
 //get all the routes
-const artistRoute = require('./routes/artists');
-const genresRoutes = require('./routes/genresRoute');
+const artistRoute = require("./routes/artists");
+const genresRoutes = require("./routes/genresRoute");
 //when you search for /artist handle response in artist router
-app.use('/artists' , artistRoute);
+app.use("/artists", artistRoute);
 //handling genres route
-app.use('/genres' , genresRoutes)
+app.use("/genres", genresRoutes);
 //show endpoints when on homepage
-app.get('/', function(req, res){
-    res.status(200).json({
-        message: "it works!",
-        endPoints: [
-            {
-                method: "GET",
-                endPoints:[
-                    "/artists", 
-                    "/artists/{id}",
-                    "/genres",
-                    "genres/{id}"
-
-                ]
-            },
-            {
-                method: "POST",
-                endPoints:[
-                    "/artists",
-                    "/genres",
-                    "/genres/{id}"
-                ]
-            },
-            {
-                method: "Delete",
-                endPoints:[
-                    "/artists/{id}",
-                    "/genres/{id}"
-                ]
-            },
-            {
-                method: "PUT",
-                endPoints:[
-                    "/artists/{id}"
-                ]
-            }
-        ]
-    });
+app.get("/", function(req, res) {
+  res.status(200).json({
+    message: "it works!",
+    endPoints: [
+      {
+        method: "GET",
+        endPoints: ["/artists", "/artists/{id}", "/genres", "genres/{id}"]
+      },
+      {
+        method: "POST",
+        endPoints: ["/artists", "/genres", "/genres/{id}"]
+      },
+      {
+        method: "Delete",
+        endPoints: ["/artists/{id}", "/genres/{id}"]
+      },
+      {
+        method: "PUT",
+        endPoints: ["/artists/{id}"]
+      }
+    ]
+  });
 });
 
-module.exports = app
+module.exports = app;
--- routes/artists.js
+++ routes/artists.js
@@ -1,41 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Artist = require('../models/artistModel');
+const Artist = require("../models/artistModel");
 //handling get request for /artists and send back all artist
-router.get('/', (req, res , next) =>{
+router.get("/", (req, res, next) => {
     Artist.find({}, (err, artists) => {
         res.status(200).send(artists);
     });
 });
 
 //handling post request for /artist
-router.post('/' , (req, res, next) => {
+router.post("/", (req, res, next) => {
     var artist = new Artist(req.body);
-    artist.save((e) => {
+    artist.save(e => {
         artist.save();
         res.status(201).send(req.body);
-    })
-})
+    });
+});
 
 //return artist based on id
-router.get("/:artistId" , (req, res, next) => {
+router.get("/:artistId", (req, res, next) => {
     //you get the id from using the req.params.{id name in url}
     const id = req.params.artistId;
     res.status(200).json({
         id: id,
-        message: "it worked",
-    })
+        message: "it worked"
+    });
 });
 
 //route to patch artist
-router.patch("/:artistId" , (req, res, next) => {
+router.patch("/:artistId", (req, res, next) => {
     res.status(200).json({
         message: "this is patch"
     });
 });
 
 //route to delete artist
-router.delete("/:artistId" , (req, res, next) => {
+router.delete("/:artistId", (req, res, next) => {
     res.status(200).json({
         message: "this is delete"
     });
