f1e1a6713a0c97a1d7eb67bd14e87f59bbf3f340
Roel Voordendag <rvoordendag@gmail.com>
2019-09-19 22:59:16 +0200

    added extra validation on adding artist id

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -14,13 +14,17 @@ router.get("/:userid", (req, res, next) => {
 //add artist id to user artist id => Spotify id
 router.post("/:userid", (req, res, next) => {
     const userid = req.params.userid;
+    console.log(typeof req.body.artistid);
     //find user to update
-    if (req.body.artistid) {
+    if (req.body.artistid && typeof req.body.artistid == "number") {
         User.findOne({ _id: userid }, (err, user) => {
             user.artistsID.push(req.body.artistid);
             user.save();
             res.status(202).send("Artist has been added");
-        });
+        }).catch(e => res.status(404).send("User is not found"));
+    } else {
+        // catch the fact that artistid is not filled in
+        res.status(400).send("There is no artistid. Check the body your posting with.");
     }
 });
 //remove user
