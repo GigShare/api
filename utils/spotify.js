bec74500d58c13f4ad065642978703efad460d17
Roel Voordendag <roelvoordendag@Roels-MacBook-Pro.local>
2019-09-30 22:10:37 +0200

    prettier changes

--- utils/spotify.js
+++ utils/spotify.js
@@ -1,28 +1,32 @@
-const config = require('../config');
-const fetch = require('node-fetch');
-const modelArtist = require('../models/artistModel');
+const config = require("../config");
+const fetch = require("node-fetch");
+const modelArtist = require("../models/artistModel");
 module.exports = {
     //This functions gets the artist your search for
     //and check if you even have the right name
-    checkArtist: function(nameArtist) {  
+    checkArtist: function(nameArtist) {
         //check for spaces in artists name!
         //regex!
-        fetch('https://api.spotify.com/v1/search?q=slaves&type=artist', {
-        method: 'GET', // or 'PUT'
-        headers:{
-          'Authorization': 'Bearer ' + config.spotifyKey, 
-        }}).then((res) => {
-            if(res.status === 401){
-                this.getAuthorization(nameArtist);
-                throw 'Getting a new token';
-            }else{
-                return res.json();
+        fetch("https://api.spotify.com/v1/search?q=slaves&type=artist", {
+            method: "GET", // or 'PUT'
+            headers: {
+                Authorization: "Bearer " + config.spotifyKey
             }
-        }).then((response) => {
-            console.log('Success:', JSON.stringify(response))
-        }).catch(error => console.error('Error:', error)); 
+        })
+            .then(res => {
+                if (res.status === 401) {
+                    this.getAuthorization(nameArtist);
+                    throw "Getting a new token";
+                } else {
+                    return res.json();
+                }
+            })
+            .then(response => {
+                console.log("Success:", JSON.stringify(response));
+            })
+            .catch(error => console.error("Error:", error));
     },
-    getAuthorization : function(artist){
-        console.log(artist + ' here i will get a new token');
+    getAuthorization: function(artist) {
+        console.log(artist + " here i will get a new token");
     }
-}
+};
