af33a0356e9d3c46835f700bc505c1a4d04dbab2
Roel <rvoordendag@gmail.com>
2020-02-17 21:13:41 +0100

    cganges

--- routes/artistsRoute.js
+++ routes/artistsRoute.js
@@ -1,15 +1,16 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Artist = require("../models/artistModel");
+const Artist = require('../models/artistModel');
+
 //handling get request for /artists and send back all artist
-router.get("/", (req, res, next) => {
+router.get('/', (req, res, next) => {
     Artist.find({}, (err, artists) => {
         res.status(200).send(artists);
     });
 });
 
 //handling post request for /artist
-router.post("/", (req, res, next) => {
+router.post('/', (req, res, next) => {
     var artist = new Artist(req.body);
     artist.save(e => {
         artist.save();
@@ -18,7 +19,7 @@ router.post("/", (req, res, next) => {
 });
 
 //return artist based on id
-router.get("/:artistId", (req, res, next) => {
+router.get('/:artistId', (req, res, next) => {
     //you get the id from using the req.params.{id name in url}
     //find artist on id
     const id = req.params.artistId;
@@ -28,16 +29,16 @@ router.get("/:artistId", (req, res, next) => {
 });
 
 //route to patch artist
-router.patch("/:artistId", (req, res, next) => {
+router.patch('/:artistId', (req, res, next) => {
     res.status(200).json({
-        message: "this is patch"
+        message: 'this is patch'
     });
 });
 
 //route to delete artist
-router.delete("/:artistId", (req, res, next) => {
+router.delete('/:artistId', (req, res, next) => {
     res.status(200).json({
-        message: "this is delete"
+        message: 'this is delete'
     });
 });
 
