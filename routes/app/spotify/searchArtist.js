9dc6aa86896f73313f2ae637c3c81385c1b4e063
Roel <rvoordendag@gmail.com>
2020-11-08 16:38:39 +0100

    post route added

--- routes/app/spotify/searchArtist.js
+++ routes/app/spotify/searchArtist.js
@@ -1,11 +1,14 @@
 //here all the shit for users are gonna move
 const express = require('express');
-const { route } = require('./spotifyRoute');
 const router = express.Router({ mergeParams: true });
+
+router.post('/', (req, res, next) => {
+    res.status(201).send({ 'bigt iddies': true });
+});
 
 //handling search request for artist
 router.get('/', (req, res, next) => {
-    res.status(201).send({ 'Find Artist': true });
+    res.status(201).send('nice');
 });
 
 module.exports = router;
