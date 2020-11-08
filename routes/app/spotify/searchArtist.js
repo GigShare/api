7d039610a9f6fedc5908fc5e3c3c5e0ceb4a6776
Roel <rvoordendag@gmail.com>
2020-11-08 17:41:55 +0100

    search route to search for artist based on app input

--- routes/app/spotify/searchArtist.js
+++ routes/app/spotify/searchArtist.js
@@ -1,11 +1,24 @@
 //here all the shit for users are gonna move
 const express = require('express');
+const fetch = require('node-fetch');
 const router = express.Router({ mergeParams: true });
 
 router.post('/', (req, res, next) => {
-    res.status(201).send({ 'bigt iddies': true });
+    const searchTerm = req.body.searchInput;
+    const url = encodeURI('https://api.spotify.com/v1/search?type=artist&query=' + searchTerm);
+    //  prettier-ignore
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            Authorization: 'Bearer ' + req.body.bearer,
+        },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        res.status(200).send(data)
+    })
 });
-
 //handling search request for artist
 router.get('/', (req, res, next) => {
     res.status(201).send('nice');
