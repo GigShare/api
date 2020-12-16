33fc1c32f7bda436635df34a475c3bff1ed2d83b
Roel <rvoordendag@gmail.com>
2020-12-16 20:38:03 +0100

    new route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 //handling get request for /artists and send back all artist
 router.use('/app', require('./app/appRoute'));
 router.use('/users', require('./users/usersRoute'));
+router.use('/concerts', require('./concerts/concerRoutes'));
 
 module.exports = router;
